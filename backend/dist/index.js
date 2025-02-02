dff99155eca51ec5c3fbb078e91481e62d44ed22
nikit <snikit086@gmail.com>
2025-02-02 14:22:31 +0530

    improvments in server

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,8 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const ws_1 = require("ws");
+const dotenv_1 = __importDefault(require("dotenv"));
+const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const http_1 = __importDefault(require("http"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
-const httpServer = app.listen(8080);
+app.use(express_1.default.json());
+app.use("/api/users", userRoutes_1.default);
+const httpServer = http_1.default.createServer(app);
+httpServer.listen(8080, () => {
+    console.log("server is running on http://localhost:8080");
+});
 const wss = new ws_1.WebSocketServer({ server: httpServer });
 wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
--- backend/src/index.tsx
+++ backend/src/index.tsx
@@ -2,14 +2,17 @@ import express from 'express'
 import { WebSocketServer , WebSocket } from 'ws'
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes"
+import http from "http";
 
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use("/api/users", userRoutes);
-const httpServer = app.listen(8080)
-
+const httpServer = http.createServer(app)
+httpServer.listen(8080 , () => {
+  console.log("server is running on http://localhost:8080")
+})
 const wss = new WebSocketServer({ server: httpServer });
 
 wss.on('connection', function connection(ws) {
